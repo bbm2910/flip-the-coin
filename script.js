3b7f084edcdc977067e572b7f86cc143b5f412db
bobby2910 <b.mastacan@gmail.com>
2025-04-05 09:06:09 +0100

    Add js functionality

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+//global variables
 const btn = document.querySelector("button");
+const result = document.querySelector(".result");
+const headsCount = document.querySelector(".headsCount");
+const tailsCount = document.querySelector(".tailsCount");
 
+//create an object to trakc the results
+const obj = { record: [], headsCount: 0, tailsCount: 0 };
+
+function coinFlip() {
+  if (Math.floor(Math.random() * 2 + 1) === 1) {
+    obj.record.push("Heads");
+    obj.headsCount++;
+    result.textContent = "Heads!";
+  } else {
+    obj.record.push("Tails");
+    obj.tailsCount++;
+    result.textContent = "Tails!";
+  }
+
+  headsCount.textContent = obj.headsCount;
+  tailsCount.textContent = obj.tailsCount;
+}
+
+//spin the coin
 btn.addEventListener("click", () => {
-  console.log(12);
+  console.log(coinFlip(1));
 });
